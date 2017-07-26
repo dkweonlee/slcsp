1b316356e35b6de2da9b0cdb4b083bd3e9cef9e2
Daniel <dkweonlee@gmail.com>
2017-07-25 22:56:04 -0400

    check zipcode in zips

--- slcsp.rb
+++ slcsp.rb
@@ -1,14 +1,27 @@
 require 'csv'
 require 'pry'
+class SLCSP_FINDER
+  puts 'Please enter data name'
+  filename = gets.chomp
+  zipcode = CSV.read("#{filename}.csv")
+  
+  # def list_zipcodes
+  i = 1
+  list_zip = []
+  while i < zipcode.length
+    list_zip << zipcode[i][0]
+    i+=1
+  end
+  # end
+  
+  zips = CSV.read("zips.csv").length
+  binding.pry
+  j = 0
+  while i < CSV.read("zips.csv").length
+    
+  end
 
-puts 'Please enter data name'
-filename = gets.chomp
-zipcode = CSV.read("#{filename}.csv")
-i = 1
-list_zip = []
-while i < zipcode.length
-  list_zip << zipcode[i][0]
-  i+=1
 end
+
 
 binding.pry
